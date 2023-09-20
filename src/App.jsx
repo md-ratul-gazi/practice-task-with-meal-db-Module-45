b80f58b50b724ce634a7e4fd15215b48772c52e9
md-ratul-gazi <mdratulgazi001@gmail.com>
2023-09-20 20:58:17 +0600

    Install react in this project and tailwind setup done

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Practice Task with mealdb (Module 45)</h1>
+    </>
+  );
+}
+
+export default App;
